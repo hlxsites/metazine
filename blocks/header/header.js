a324bd8a6cec2dde433bf44d83776c7ad5425729
dprevelige <dpreveli@adobe.com>
2023-03-14 12:58:17 -0400

    Update header.js for navPath

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -92,7 +92,8 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
 export default async function decorate(block) {
   // fetch nav content
   const navMeta = getMetadata('nav');
-  const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
+  //const navPath = navMeta ? new URL(navMeta).pathname : '/nav';
+  const navPath = navMeta ? navMeta : '/nav';
   const resp = await fetch(`${navPath}.plain.html`);
 
   if (resp.ok) {
