9d721ae95bd61b0c858d6083e62b21b9dbf7ccef
dprevelige <dpreveli@adobe.com>
2023-03-14 15:02:15 -0400

    Update image.js

--- blocks/image/image.js
+++ blocks/image/image.js
@@ -6,8 +6,12 @@
 export default async function decorate(block) {
     const link = block.querySelector('a');
     const path = link ? link.getAttribute('href') : block.textContent.trim();
+    const alt = block.querySelector('h3');
+    const title = block.querySelector('h2');
     const img = document.createElement('img');
     img.setAttribute('src',path)
+    img.setAttribute('alt',alt.textContent());
     block.textContent = '';
     block.append(img);
+    block.append(title);
 }
